15863c6f97e130d64f63ffb2033eaa059736425d
asd <zhuzemin4@hotmail.com>
2019-12-27 16:41:55 +0800

    add: Video && Image Button

---
+++ README.md
@@ -0,0 +1,7 @@
+# google_search_filter_with_Language
+
+
+https://greasyfork.org/en/scripts/392869-google-search-filter-with-language/
+
+![effect](https://github.com/zhuzemin/Google-search-filter-with-language/raw/master/Screenshot-2019-12-27.jpg)
+
---
+++ google_search_filter_with_Language.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name           Google_search_filter_with_Language
+// @description    Google搜索结果以语言过滤
+// @namespace      Google_search_filter_with_Language
+// @supportURL     https://github.com/zhuzemin
+// @include        https://www.google.*/search*
+// @author         zhuzemin
+// @version        1.1
+// @grant		   none
+// ==/UserScript==
+var lr = document.createElement("lr");
+lr.innerHTML = '<lr><a href="'+window.location.href.replace(/lr=lang_(.{1,16})/g, '').replace(/$/, '&lr=lang_en')+'">EN</a>/<a href="'+window.location.href.replace(/lr=lang_(.{1,16})/g, '').replace(/$/, '&lr=lang_ja')+'">JA</a>/<a href="'+window.location.href.replace(/lr=lang_(.{1,16})/g, '').replace(/$/, '&lr=lang_zh-TW')+'">TW</a>/<a href="'+window.location.href.replace(/lr=lang_(.{1,16})/g, '').replace(/$/, '&lr=lang_zh-CN')+'">CN</a>/<a href="'+window.location.href.replace(/lr=lang_(.{1,16})/g, '').replace(/$/, '&lr=lang_zh-CN|lang_zh-TW')+'">TW&CN</a>/<a href="'+window.location.href.replace(/lr=lang_(.{1,16})/g, '')+'">All Language</a></lr>'
+
+var logo=document.getElementsByClassName("Tg7LZd")[0]
+logo.parentNode.insertBefore(lr, logo);
+var links=document.querySelectorAll("a.q.qs");
+for(var link of links){
+  if(link.innerText.match(/Video|Image/)!=null){
+    logo.parentNode.insertBefore(link, logo);
+  }
+}
+/*var hdtbSum=document.querySelector("#hdtbSum");
+var hdtbMenus=document.querySelector("#hdtbMenus");
+logo.parentNode.insertBefore(hdtbMenus, logo);
+var searchform=document.querySelector("#searchform");
+searchform.insertBefore(hdtbSum, null);*/
